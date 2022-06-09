56e8deb49f184d610982ec9c1aa73c643173e83d
Ashwini Raikar <ashwiniraikar715@gmail.com>
2022-06-09 15:13:18 +0530

    Ticket 10000:Available

---
+++ Maths/sum.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	int sum =0,i,j;
+	sum=i+j;
+	printf("Enter the value");
+	scanf(%d,%d"&i,&j");
+	return 0;	i
+}
---
+++ someUselessFile.c
@@ -0,0 +1,5 @@
+hello dabba file 
+waste file
+good file 
+bad file
+"Hello world"
